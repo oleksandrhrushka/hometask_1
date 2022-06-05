2ff9fbcfd607cffb7a97bd3abf1e0fcb7f6da536
Oleksandr Hrushka <oleksandr.hrushka@gmail.com>
2022-06-05 16:56:31 +0300

    This is the first commit.

--- main.go
+++ main.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello there! This is the original file without any modifications.")
+	fmt.Println("- This is the file amendment after I've swithed to a new MASTER branch.")
 }
