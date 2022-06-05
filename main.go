2acc545f9750324ea93d87dfba302351d75a2266
Oleksandr Hrushka <oleksandr.hrushka@gmail.com>
2022-06-05 17:03:10 +0300

    This is the second commit.

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello there! This is the original file without any modifications.")
-	fmt.Println("- This is the file amendment after I've swithed to a new MASTER branch.")
+	fmt.Println("- This is the file amendment after I've switched to a new MASTER branch.")
+	fmt.Println("- Here I've switched to a new branch STARS")
 }
