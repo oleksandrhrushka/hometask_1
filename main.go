20ab6d9125105b25def62cb75e386131a93c0209
Oleksandr Hrushka <oleksandr.hrushka@gmail.com>
2022-06-06 12:10:43 +0300

    Last but not least

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello there! This is the original file without any modifications.")
 	fmt.Println("- This is the file amendment after I've switched to a new MASTER branch.")
-	fmt.Println("- Here I've switched to a new branch STARS")
-	fmt.Println("- This one goes to branch FEATHERS")
+	fmt.Println("- Here I've switched to a new branch STARS.")
+	fmt.Println("- This one goes to branch FEATHERS.")
+	fmt.Println("- Last but not least. This one is for WASP branch.")
 }
